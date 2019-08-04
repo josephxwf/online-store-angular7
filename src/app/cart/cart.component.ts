96a00e9ea645c0b809e287a4ba65807709f6f40a
joseph fan <josephxwf@gmail.com>
2019-08-03 21:23:17 -0400

    defitn checkout form property and onsubmit method

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -10,14 +10,28 @@ import { FormBuilder } from '@angular/forms';
 export class CartComponent implements OnInit {
 
   items;
+  checkoutForm;
   constructor(
     private cartService: CartService,
     private formBuilder: FormBuilder,
   ) {
 
     this.items = this.cartService.getItems();
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
    }
 
+  
+  onSubmit(customerData) {
+    // Process checkout data here
+    console.warn('Your order has been submitted', customerData);
+
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+  }
+  
  
 
   ngOnInit() {
